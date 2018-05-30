71c2ed61a267da5dfd5f80bd8d9f1da33a650d11
Manuel7568 <161wit17@o365.htl-leoben.at>
2018-05-30 05:57:37 -0700

    wuerfel-simulator

--- wuerfel.py
+++ wuerfel.py
@@ -1,2 +1,32 @@
 # wuerfel.py
 from random import *
+
+anz1 = 0
+anz2 = 0
+anz3 = 0
+anz4 = 0
+anz5 = 0
+anz6 = 0
+
+for i in range(0,100):
+    rnd = randint(1,6)
+    if(rnd == 1):
+        anz1 += 1
+    if(rnd == 2):
+        anz2 += 1
+    if(rnd == 3):
+        anz3 += 1
+    if(rnd == 4):
+        anz4 += 1
+    if(rnd == 5):
+        anz5 += 1
+    if(rnd == 6):
+        anz6 += 1
+
+
+print("Anzahl der gewürfelten Einser: " , anz1)
+print("Anzahl der gewürfelten Zweier: " , anz2)
+print("Anzahl der gewürfelten Dreier: " , anz3)
+print("Anzahl der gewürfelten Vierer: " , anz4)
+print("Anzahl der gewürfelten Fünfer: " , anz5)
+print("Anzahl der gewürfelten Sechser: " , anz6)        
